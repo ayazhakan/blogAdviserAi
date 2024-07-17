700958d88a172ff35eb0d04922f2ca47ecc4be90
Hakan AYAZ <hakanayaz9844@outlook.com>
2024-07-17 21:58:52 +0300

    project created

---
+++ src/main/java/com/blogAdviserAi/blogAdviserAi/BlogAdviserAiApplication.java
@@ -0,0 +1,13 @@
+package com.blogAdviserAi.blogAdviserAi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogAdviserAiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogAdviserAiApplication.class, args);
+	}
+
+}
